6c2b673abd4201ffc14fdd85137993cd1da38a02
Maryna Kulenkova <Marina.Kulenkova@dataart.com>
2021-02-24 20:34:57 +0200

    Initial Commit

---
+++ Trekr/TrekrApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrekrApp.swift
+//  Trekr
+//
+//  Created by Marina on 24.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TrekrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
